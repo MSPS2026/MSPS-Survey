a107d72c57d7f9dd82d89834f0a9559e48164149
LiamMananghaya <lgjuly2006@gmail.com>
2026-02-08 23:10:41 -0600

    Update VD 26.py

--- VD 26.py
+++ VD 26.py
@@ -33,10 +33,11 @@ elif st.session_state.step == 2:
     if st.button("Next"):
         st.session_state.location = location
         st.session_state.step = 3
-        st.rerun
+        st.rerun()
 
 # Final page
 elif st.session_state.step == 3:
     st.success("Survey Complete! On behalf of the University of Manitoba's Most Beautiful Girl's Boyfriend (UMMBGB), we thanks for expressing an interest in learning about penguins! Here is a summary of your answers:")
     st.write("Species:", st.session_state.species)
     st.write("Location:", st.session_state.location)
+
